870bf480092a3a3055af47dbb8eb946de4118b8d
Cefn Hoile <github.com@cefn.com>
2021-08-30 14:04:31 +0100

    Restructure recursion to avoid duplicate npm retrieval

--- src/package.ts
+++ src/package.ts
@@ -5,7 +5,7 @@ import pLimit from 'p-limit';
 
 const connectionPool = pLimit(32);
 
-export async function getNpmData(name: string): Promise<NPMPackage | null> {
+export async function getNPMPackage(name: string): Promise<NPMPackage | null> {
   const request = connectionPool(() => got(`https://registry.npmjs.org/${name}`, { retry: 0, throwHttpErrors: false }));
   // throws only if no server response
   const { statusCode, body } = await request;
@@ -18,38 +18,37 @@ export async function getNpmData(name: string): Promise<NPMPackage | null> {
   throw new Error(`Server rejected with status:${statusCode}\n${body}`);
 }
 
-export async function getDependencies(name: string, version: string): Promise<NPMDependencies | null> {
-  const npmData = await getNpmData(name);
-  if (npmData) {
-    const npmVersionData = npmData?.versions?.[version];
-    if (npmVersionData) {
-      return npmVersionData.dependencies || {};
-    }
+export async function getDependencies(npmPackage: NPMPackage, version: string): Promise<NPMDependencies | null> {
+  const npmVersionData = npmPackage?.versions?.[version];
+  if (npmVersionData) {
+    return npmVersionData.dependencies || {};
   }
   return null;
 }
 
-export async function getDependenciesDeep(name: string, version: string): Promise<NPMDependencies | null> {
+export async function getDependenciesDeep(npmPackage: NPMPackage, version: string): Promise<NPMDependencies | null> {
   // TODO unroll to avoid duplicated call for each package to getNpmData() unless memoized
-  const dependencies = await getDependencies(name, version);
+  const dependencies = await getDependencies(npmPackage, version);
   if (dependencies) {
     // traverse list, triggering retrievals in parallel (limited by connectionPool)
     const dependencyRecords = await Promise.all(
       Object.entries(dependencies).map(async ([ desiredName, desiredVersion ]) => {
-        const desiredEntry = await getNpmData(desiredName);
-        const availableVersions = desiredEntry?.versions;
-        if (availableVersions) {
-          const maxVersion = maxSatisfying(Object.keys(availableVersions), desiredVersion);
-          if (maxVersion) {
-            const maxVersionDependencies = await getDependenciesDeep(desiredName, maxVersion);
-            return {
-              name: desiredName,
-              version: maxVersion,
-              dependencies: maxVersionDependencies,
-            };
+        const desiredPackage = await getNPMPackage(desiredName);
+        if (desiredPackage) {
+          const availableVersions = desiredPackage?.versions;
+          if (availableVersions) {
+            const maxVersion = maxSatisfying(Object.keys(availableVersions), desiredVersion);
+            if (maxVersion) {
+              const maxVersionDependencies = await getDependenciesDeep(desiredPackage, maxVersion);
+              return {
+                name: desiredName,
+                version: maxVersion,
+                dependencies: maxVersionDependencies,
+              };
+            }
           }
         }
-        throw new Error(`Package ${name} has non-existent dependency ${desiredName}:${desiredVersion}`);
+        throw new Error(`Package ${npmPackage.name} has non-existent dependency ${desiredName}:${desiredVersion}`);
       })
     );
     // transform list to map
--- test/package.test.ts
+++ test/package.test.ts
@@ -1,14 +1,13 @@
+import * as assert from 'assert';
 import { nockRecord } from './util';
-import { getDependencies, getDependenciesDeep, getNpmData } from '../src/package';
+import { getDependencies, getDependenciesDeep, getNPMPackage } from '../src/package';
 
 describe('NPM retrieval functions', () => {
   describe('getNpmData()', () => {
     it('retrieves data if package version exists', async () => {
       const { nockDone } = await nockRecord('getNpmData_present');
-      const data = await getNpmData('@lauf/store-react');
-      if (data === null) {
-        throw new Error('Should retrieve data');
-      }
+      const data = await getNPMPackage('@lauf/store-react');
+      assert(data !== null, 'Should retrieve data');
       expect(data.name).toBe('@lauf/store-react');
       expect((data as unknown as { author: { name: string } })?.author?.name).toBe('Cefn Hoile');
       nockDone();
@@ -16,7 +15,7 @@ describe('NPM retrieval functions', () => {
 
     it('returns null for non-existent package', async () => {
       const { nockDone } = await nockRecord('getNpmData_absent');
-      const data = await getNpmData('nothingburger42');
+      const data = await getNPMPackage('nothingburger42');
       expect(data).toBe(null);
       nockDone();
     });
@@ -25,11 +24,10 @@ describe('NPM retrieval functions', () => {
   describe('getDependencies()', () => {
     it('retrieves data if package version exists', async () => {
       const { nockDone } = await nockRecord('getDependencies_present');
-      const data = await getDependencies('@lauf/store-react', '1.0.1');
-      if (data === null) {
-        throw new Error('Should retrieve data');
-      }
-      expect(data).toMatchInlineSnapshot(`
+      const npmPackage = await getNPMPackage('@lauf/store-react');
+      assert(npmPackage !== null, 'Should retrieve data');
+      const result = await getDependencies(npmPackage, '1.0.1');
+      expect(result).toMatchInlineSnapshot(`
         Object {
           "@lauf/store": "^1.0.1",
         }
@@ -37,10 +35,12 @@ describe('NPM retrieval functions', () => {
       nockDone();
     });
 
-    it('returns null for non-existent package', async () => {
+    it('returns null for non-existent package version', async () => {
       const { nockDone } = await nockRecord('getDependencies_absent');
-      const data = await getDependencies('nothingburger42', '42.42.42');
-      expect(data).toBe(null);
+      const npmPackage = await getNPMPackage('@lauf/store-react');
+      assert(npmPackage !== null, 'Should retrieve data');
+      const result = await getDependencies(npmPackage, '42.42.42');
+      expect(result).toBe(null);
       nockDone();
     });
   });
@@ -48,11 +48,10 @@ describe('NPM retrieval functions', () => {
   describe('getDependenciesDeep()', () => {
     it('retrieves data if package version exists', async () => {
       const { nockDone } = await nockRecord('getDependenciesDeep_present');
-      const data = await getDependenciesDeep('@lauf/store-react', '1.0.1');
-      if (data === null) {
-        throw new Error('Should retrieve data');
-      }
-      expect(data).toMatchInlineSnapshot(`
+      const npmPackage = await getNPMPackage('@lauf/store-react');
+      assert(npmPackage !== null, 'Should retrieve data');
+      const result = await getDependenciesDeep(npmPackage, '1.0.1');
+      expect(result).toMatchInlineSnapshot(`
         Object {
           "@lauf/store": Object {
             "dependencies": Object {
@@ -68,10 +67,12 @@ describe('NPM retrieval functions', () => {
       nockDone();
     });
 
-    it('returns null for non-existent package', async () => {
-      const { nockDone } = await nockRecord('getDependenciesDeep_absent');
-      const data = await getDependenciesDeep('nothingburger42', '42.42.42');
-      expect(data).toBe(null);
+    it('returns null for non-existent package version', async () => {
+      const { nockDone } = await nockRecord('getDependencies_absent');
+      const npmPackage = await getNPMPackage('@lauf/store-react');
+      assert(npmPackage !== null, 'Should retrieve data');
+      const result = await getDependenciesDeep(npmPackage, '42.42.42');
+      expect(result).toBe(null);
       nockDone();
     });
   });
